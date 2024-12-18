e18ee378c2bc41de952ae040f7bae946b648b662
Konstantin <163849606+it2konst@users.noreply.github.com>
2024-12-18 18:03:20 +0700

    MenuMob added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,52 +1,34 @@
 <script setup>
 import SvgHeader from './blocks/icons/SvgHeader.vue'
-
+import MenuMob from './blocks/MenuMob.vue'
 import { ref } from 'vue'
 
 const isBurger = ref(false)
 
 const toggleBurger = () => {
     isBurger.value = !isBurger.value
-    // window.scrollTo({
-    //     top: 0,
-    //     behavior: 'smooth',
-    // })
 }
 </script>
 
 <template>
+    <!-- Component MenuMob-->
+    <MenuMob v-show="isBurger" @toggleBurger="toggleBurger" :is-burger="isBurger" />
+
     <div class="header__fixed-wrap d-flex">
-        <div class="header__logo d-flex">
+        <div class="header__logo d-flex mob-tap">
             <SvgHeader name="icon-logo" size="26" />
             <p>CICCC</p>
         </div>
-        <nav class="header__nav d-flex mob-tap">
-            <ul class="header__menu d-flex">
-                <li class="header__item"><a href="#!" class="header__link">Home</a></li>
+        <nav class="header__nav d-flex">
+            <ul class="header__menu d-flex mob-tap">
+                <li class="header__item"><a href="#header" class="header__link">Home</a></li>
                 <li class="header__item"><a href="#!" class="header__link">Courses</a></li>
                 <li class="header__item"><a href="#!" class="header__link">About</a></li>
                 <li class="header__item"><a href="#!" class="header__link">Contacts</a></li>
             </ul>
-            <transition name="menu-mob">
-                <ul v-show="isBurger" class="header__menu-mob d-flex" :class="{ active: isBurger }">
-                    <li class="header__item" @click="toggleBurger">
-                        <a href="#header" class="header__link">Home</a>
-                    </li>
-                    <li class="header__item" @click="toggleBurger">
-                        <a href="#!" class="header__link">Courses</a>
-                    </li>
-                    <li class="header__item" @click="toggleBurger">
-                        <a href="#!" class="header__link">About</a>
-                    </li>
-                    <li class="header__item" @click="toggleBurger">
-                        <a href="#!" class="header__link">Contacts</a>
-                    </li>
-                </ul>
-            </transition>
         </nav>
         <div class="header__burger mob-tap" :class="{ active: isBurger }">
-            <SvgHeader name="icon-burger" size="26" @click="isBurger = !isBurger" />
-            <div class="header__overlay" @click="isBurger = !isBurger"></div>
+            <SvgHeader name="icon-burger" size="26" @click="toggleBurger" />
         </div>
     </div>
 </template>
@@ -57,19 +39,15 @@ const toggleBurger = () => {
     .header__fixed-wrap {
         //---d-flex-center---
         position: fixed;
-        // inset: 0;
-        // left: 0;
-        // right: 0;
-        transform: translateY(-20%);
-        // transform: translateY(30%); // Уменьшаем высоту при прокрутке.
+        padding-top: remClamp(23, 30);
+        padding-bottom: remClamp(10, 20);
+        transform: translateY(-20%); // Уменьшаем высоту при прокрутке.
+
         border-bottom-left-radius: 1rem;
         border-bottom-right-radius: 1rem;
 
         background-color: #eaf3ffdd;
         backdrop-filter: blur(0.1rem);
-
-        padding-top: remClamp(23, 30);
-        padding-bottom: remClamp(10, 20);
     }
 }
 
@@ -82,12 +60,8 @@ const toggleBurger = () => {
     gap: rem(10);
     max-width: calc(remClamp(320, 1230) - rem(30));
     width: 100%;
-
-    margin: 0 auto;
     padding-inline: rem(15);
-    // padding-top: remClamp(23, 30);
-    // padding-bottom: remClamp(10, 16);
-    // padding-inline: rem(15);
+    margin: 0 auto;
 
     transform: translateY(0);
     transition: transform 0.3s ease-in;
@@ -113,7 +87,6 @@ const toggleBurger = () => {
 
     .header__nav {
         //---d-flex---
-        // margin-left: auto;
         .header__menu {
             //---d-flex---
             flex-wrap: wrap;
@@ -125,7 +98,6 @@ const toggleBurger = () => {
                 font-family: var(--font-family);
                 font-weight: 400;
                 font-size: remClamp(14, 18);
-                // font-size: 14px;
                 line-height: rem(26);
                 color: var(--text-paragraph);
 
@@ -178,104 +150,10 @@ const toggleBurger = () => {
                 }
             }
         }
-
-        .header__menu-mob {
-            position: fixed;
-            z-index: 12;
-            top: 0;
-            left: -0.5rem;
-            // top: -1rem;
-            // left: -1rem;
-            //---d-flex---
-            flex-direction: column;
-            gap: 0.6rem;
-
-            max-width: 17rem;
-            width: 100%;
-            padding: 2rem;
-
-            background-color: var(--bg-card);
-
-            border-bottom-right-radius: 2rem;
-            box-shadow:
-                rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
-                rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
-                rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
-
-            .header__item {
-                width: 100%;
-                height: 2.2rem;
-                border-radius: 2rem;
-
-                text-shadow:
-                    0px 3px 2px rgba(0, 0, 0, 0.2),
-                    0px 6px 8px rgba(0, 0, 0, 0.2),
-                    0px 12px 24px rgba(0, 0, 0, 0.2);
-
-                transition:
-                    background-color 0.2s ease-in,
-                    box-shadow 0.2s ease-in;
-                @include hover-media {
-                    background-color: var(--bg-card-lighten);
-                    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
-                }
-                &:active {
-                    background-color: var(--bg-card-lighten);
-                    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
-                }
-            }
-
-            .header__link {
-                display: block;
-                max-width: 100%;
-                text-align: center;
-
-                font-family: var(--font-family);
-                font-weight: 400;
-                font-size: rem(20);
-                line-height: 2.2rem;
-                color: var(--text-paragraph);
-                text-transform: uppercase;
-            }
-
-            &::before,
-            &::after {
-                content: '';
-                display: block;
-                height: 0.2rem;
-                width: 80%;
-                background-color: var(--bg-card-lighten);
-            }
-
-            &.active {
-                &::before {
-                    width: 0;
-                    animation: move 0.4s ease-in 0.2s forwards;
-                }
-
-                &::after {
-                    width: 0;
-                    animation: move 0.4s ease-in 0.4s forwards;
-                }
-            }
-        }
-
-        .menu-mob-enter-active,
-        .menu-mob-leave-active {
-            transition: 0.5s ease all;
-        }
-        .menu-mob-enter-from,
-        .menu-mob-leave-to {
-            transform: translateX(-20rem);
-        }
-        .menu-mob-enter-to {
-            transform: translateX(0);
-        }
     }
 
     @include media(md) {
         .header__nav {
-            margin-left: unset;
             .header__menu {
                 display: none;
             }
@@ -284,7 +162,6 @@ const toggleBurger = () => {
         .header__burger {
             position: relative;
             display: block;
-            // margin-left: auto;
             cursor: pointer;
 
             > div:first-of-type {
@@ -308,40 +185,8 @@ const toggleBurger = () => {
                 > div:first-of-type {
                     transform: rotate(180deg) scale(1.1);
                 }
-
-                .header__overlay {
-                    position: fixed;
-                    z-index: -1;
-                    top: 0;
-                    left: -0.5rem;
-                    width: 100vw;
-                    height: 110vh;
-
-                    background-color: rgba(0, 0, 0, 0.1);
-
-                    opacity: 0;
-                    animation: opacity 0.4s ease-in 0.2s forwards;
-                }
             }
         }
-    }
-}
-
-@keyframes move {
-    from {
-        width: 0%;
-    }
-    to {
-        width: 80%;
-    }
-}
-
-@keyframes opacity {
-    from {
-        opacity: 0;
-    }
-    to {
-        opacity: 100%;
     }
 }
 </style>
---
+++ src/components/blocks/MenuMob.vue
@@ -0,0 +1,151 @@
+<script setup>
+const emit = defineEmits(['toggleBurger'])
+
+const props = defineProps({
+    isBurger: Boolean,
+})
+</script>
+
+<template>
+    <transition name="menu-mob">
+        <div class="menu-mob__overlay" :class="{ 'menu-mob__overlay--active': props.isBurger }" @click="emit('toggleBurger')">
+            <ul class="menu-mob__list d-flex mob-tap">
+                <li class="menu-mob__item">
+                    <a href="#header" class="menu-mob__link">Home</a>
+                </li>
+                <li class="menu-mob__item">
+                    <a href="#!" class="menu-mob__link">Courses</a>
+                </li>
+                <li class="menu-mob__item">
+                    <a href="#!" class="menu-mob__link">About</a>
+                </li>
+                <li class="menu-mob__item">
+                    <a href="#!" class="menu-mob__link">Contacts</a>
+                </li>
+            </ul>
+        </div>
+    </transition>
+</template>
+
+<style lang="scss">
+// ---transition menu-mob
+.menu-mob-enter-active,
+.menu-mob-leave-active {
+    transition: 0.3s ease all;
+}
+.menu-mob-enter-from,
+.menu-mob-leave-to {
+    transform: translateX(-20rem);
+    background-color: rgba(0, 0, 0, 0);
+}
+.menu-mob-enter-to {
+    transform: translateX(0);
+    background-color: rgba(0, 0, 0, 0);
+}
+// ---animations menu-mob
+@keyframes mob-overlay-bgcolor {
+    from {
+        background-color: rgba(0, 0, 0, 0);
+    }
+    to {
+        background-color: rgba(0, 0, 0, 0.2);
+    }
+}
+@keyframes mob-list-width {
+    from {
+        width: 0%;
+    }
+    to {
+        width: 80%;
+    }
+}
+// ---menu-mob
+.menu-mob__overlay {
+    // display: flex;
+    position: fixed;
+    z-index: 99;
+    inset: 0;
+    background-color: transparent;
+
+    &.menu-mob__overlay--active {
+        animation: mob-overlay-bgcolor 0.3s ease-in 0.3s forwards;
+
+        .menu-mob__list {
+            &::before {
+                width: 0;
+                animation: mob-list-width 0.3s ease-in 0.3s forwards;
+            }
+
+            &::after {
+                width: 0;
+                animation: mob-list-width 0.3s ease-in 0.5s forwards;
+            }
+        }
+    }
+
+    .menu-mob__list {
+        //---d-flex---
+        flex-direction: column;
+        gap: 0.6rem;
+
+        max-width: 17rem;
+        width: 100%;
+        padding: 2rem;
+
+        box-shadow:
+            rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
+            rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+        background-color: var(--bg-card);
+        border-bottom-right-radius: 2rem;
+
+        .menu-mob__item {
+            width: 100%;
+            height: 2.2rem;
+            border-radius: 2rem;
+
+            text-shadow:
+                0px 3px 2px rgba(0, 0, 0, 0.2),
+                0px 6px 8px rgba(0, 0, 0, 0.2),
+                0px 12px 24px rgba(0, 0, 0, 0.2);
+
+            transition:
+                background-color 0.2s ease-in,
+                box-shadow 0.2s ease-in;
+
+            &:hover {
+                @media (hover: hover) {
+                    background-color: var(--bg-card-lighten);
+                    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+                }
+            }
+
+            &:active {
+                background-color: var(--bg-card-lighten);
+            }
+        }
+
+        .menu-mob__link {
+            display: block;
+            max-width: 100%;
+            text-align: center;
+
+            font-family: var(--font-family);
+            font-weight: 400;
+            font-size: rem(20);
+            line-height: 2.2rem;
+            color: var(--text-paragraph);
+            text-transform: uppercase;
+        }
+
+        &::before,
+        &::after {
+            content: '';
+            display: block;
+            height: 0.2rem;
+            width: 80%;
+            background-color: var(--bg-card-lighten);
+        }
+    }
+}
+</style>
